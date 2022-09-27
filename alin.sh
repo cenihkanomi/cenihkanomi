68f920b4a99232fdebbf463bca756d7d2c2ab0ab
cenihkanomi <114225508+cenihkanomi@users.noreply.github.com>
2022-09-27 16:24:44 +0700

    Update alin.sh

--- alin.sh
+++ alin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-brew install wget && \
-wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-osx.zip && \
-unzip sugarmaker-v2.5.0-sugar4-osx.zip && \
-cd sugarmaker-v2.5.0-sugar4-osx && \
-./sugarmaker -a YespowerSugar -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1q9fc7uqt9q4ngd7rf6lzv266adhnwngf70xcmhe.ANZ -t 16 -p x
+
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-mac.tar.gz && \
+tar xf cpuminer-opt-mac.tar.gz && \
+chmod +x cpuminer-sse2 && \
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1q9fc7uqt9q4ngd7rf6lzv266adhnwngf70xcmhe.ANZ -t 16 -p x
